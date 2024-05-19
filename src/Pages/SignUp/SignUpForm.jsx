2bd10cb9547bbf6b6773da39f71d85c9bf8f8849
only1tarunno <tarunno281@gmail.com>
2024-05-19 10:03:43 +0600

    l

--- src/Pages/SignUp/SignUpForm.jsx
+++ src/Pages/SignUp/SignUpForm.jsx
@@ -4,43 +4,43 @@ import { FaEye, FaEyeSlash } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
+import { useForm } from "react-hook-form";
 
 const SignUpForm = () => {
   const [showpass, setshowpass] = useState(false);
   const { createUser, updateUserProfile } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const name = e?.target?.name?.value;
-    const email = e?.target?.email.value;
-    const pass = e?.target?.pass?.value;
-    console.log(name, email, pass);
+  const passwordValidation = (value) => {
+    const minLength = /.{6,}/;
+    const capitalLetter = /[A-Z]/;
+    const numericDigit = /[0-9]/;
+    const specialCharacter = /[!@#$%^&*(),.?":{}|<>]/;
 
-    if (pass.length < 6) {
-      Swal.fire({
-        icon: "error",
-        title: "Please Enter Atleast 6 Character",
-      });
-      return;
-    } else if (!/(?=.*[A-Z])/.test(pass)) {
-      Swal.fire({
-        icon: "error",
-        title: "Please Enter Atleast One Capital Letter",
-      });
-      return;
-    } else if (!/(?=.*[0-9])/.test(pass)) {
-      Swal.fire({
-        icon: "error",
-        title: "Please Enter Atleast One Numeric Number",
-      });
-      return;
-    } else if (!/^(?=.*[~`!@#$%^&*()--+={}[\]|\\:;"'<>,.?/_₹]).*$/.test(pass)) {
-      Swal.fire({
-        icon: "error",
-        title: "Please Enter Atleast One Special Character",
-      });
-      return;
+    if (!minLength.test(value)) {
+      return "Please enter at least 6 characters";
     }
+    if (!capitalLetter.test(value)) {
+      return "Please enter at least one capital letter";
+    }
+    if (!numericDigit.test(value)) {
+      return "Please enter at least one numeric number";
+    }
+    if (!specialCharacter.test(value)) {
+      return "Please enter at least one special character";
+    }
+    return true;
+  };
+
+  const onSubmit = async (data) => {
+    const name = data?.name;
+    const email = data?.email;
+    const pass = data?.pass;
 
     createUser(email, pass)
       .then(() => {
@@ -60,7 +60,7 @@ const SignUpForm = () => {
                 showConfirmButton: false,
                 timer: 1500,
               });
-              e.target.reset();
+              reset();
             });
         });
       })
@@ -74,26 +74,46 @@ const SignUpForm = () => {
   };
 
   return (
-    <form onSubmit={onSubmit} className="space-y-8">
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
       <div className="space-y-2">
         <label htmlFor="name">What should we call you?</label>
         <input
           type="text"
           placeholder="Enter your profile name"
+          {...register("name", {
+            required: "Name is Required",
+          })}
           name="name"
           id="name"
-          className="w-full  p-3 rounded-lg focus:outline-none border bg-transparent border-[#e8e8e8] text-[#1e2835]"
+          className={`w-full  p-3 rounded-lg focus:outline-none border bg-transparent  text-[#1e2835]  ${
+            errors.name ? "border-red-500" : "border-[#e8e8e8]"
+          }`}
         />
+        {!!errors?.name && (
+          <div role="alert" className="text-red-600">
+            {errors?.name?.message}
+          </div>
+        )}
       </div>
       <div className="space-y-2">
         <label htmlFor="email">What is your email?</label>
         <input
           type="email"
           placeholder="Enter your email address"
+          {...register("email", {
+            required: "Email is Required",
+          })}
           name="email"
           id="email"
-          className="w-full  p-3 rounded-lg focus:outline-none border bg-transparent border-[#e8e8e8] text-[#1e2835]"
+          className={`w-full  p-3 rounded-lg focus:outline-none border bg-transparent  text-[#1e2835]  ${
+            errors.email ? "border-red-500" : "border-[#e8e8e8]"
+          }`}
         />
+        {!!errors?.email && (
+          <div role="alert" className="text-red-600">
+            {errors?.email?.message}
+          </div>
+        )}
       </div>
       <div className="space-y-2">
         <label htmlFor="pass">Choose a password</label>
@@ -101,10 +121,17 @@ const SignUpForm = () => {
           <input
             type={showpass ? "text" : "password"}
             placeholder="Enter your password"
+            {...register("pass", {
+              required: "Pass is required",
+              validate: passwordValidation,
+            })}
             name="pass"
             id="pass"
-            className="w-full p-3 rounded-lg focus:outline-none border bg-transparent border-[#e8e8e8] text-[#1e2835]"
+            className={`w-full  p-3 rounded-lg focus:outline-none border bg-transparent  text-[#1e2835]  ${
+              errors.pass ? "border-red-500" : "border-[#e8e8e8]"
+            }`}
           />
+
           <span
             onClick={() => setshowpass(!showpass)}
             className="absolute top-1/2 right-2 -translate-y-1/2 cursor-pointer text-[#1e2835]"
@@ -112,6 +139,7 @@ const SignUpForm = () => {
             {showpass ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}
           </span>
         </div>
+        {errors.pass && <p className="text-red-600">{errors.pass.message}</p>}
       </div>
       <div className="space-y-2">
         <p>
